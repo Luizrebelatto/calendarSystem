d74caefb5bdda0a40223572da08c6c088ce618b5
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2025-06-17 22:36:47 -0300

    add icon and colors

--- src/screens/Login/login.screens.tsx
+++ src/screens/Login/login.screens.tsx
@@ -1,25 +1,32 @@
 import { useState } from "react";
 import { View, StyleSheet } from "react-native";
-import { Button, TextInput } from "react-native-paper";
+import { Button, TextInput, Avatar } from "react-native-paper";
 
 export function Login(){
-    const [text, setText] = useState("")
+    const [email, setEmail] = useState<string>("")
+    const [password, setPassword] = useState<string>("")
+
     return (
         <View style={styles.wrapper}>
+            <Avatar.Image size={150} source={require('../../assets/pet.png')} style={{ alignSelf: "center", marginBottom: 15 }}/>
             <TextInput
                 label="Email"
                 mode="outlined"
-                value={text}
-                onChangeText={text => setText(text)}
+                value={email}
+                onChangeText={email => setEmail(email)}
+                keyboardType="email-address"
+                outlineColor="#4A2E1F"
             />
             <TextInput
                 label="Password"
                 mode="outlined"
-                value={text}
-                onChangeText={text => setText(text)}
+                value={password}
+                onChangeText={password => setPassword(password)}
                 style={{ marginTop: 10 }}
+                keyboardType="default"
+                secureTextEntry={true}
             />
-            <Button mode="contained" onPress={() => console.log("press")} style={{ marginTop: 60 }}>
+            <Button mode="contained" onPress={() => console.log("press")} style={{ marginTop: 60, backgroundColor: "#D86F36" }}>
                 Login
             </Button>
        </View>
@@ -33,6 +40,6 @@ const styles = StyleSheet.create({
         alignContent: "center",
         justifyContent: "center",
         paddingHorizontal: 15,
-
+        backgroundColor: "#f2dab5"
     }
 })
