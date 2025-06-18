220c2d2b12b6c89a3ede4c35e6d83e8753644d4b
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2025-06-17 22:25:44 -0300

    styles for login page

--- src/screens/Login/login.screens.tsx
+++ src/screens/Login/login.screens.tsx
@@ -1,22 +1,23 @@
 import { useState } from "react";
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { Button, TextInput } from "react-native-paper";
+
 export function Login(){
     const [text, setText] = useState("")
     return (
-        <View style={{ flex: 1, width: "100%", alignContent: "center", justifyContent: "center" }}>
+        <View style={styles.wrapper}>
             <TextInput
                 label="Email"
                 mode="outlined"
                 value={text}
                 onChangeText={text => setText(text)}
-                selectionColor="red"
             />
             <TextInput
-                label="Email"
+                label="Password"
                 mode="outlined"
                 value={text}
                 onChangeText={text => setText(text)}
+                style={{ marginTop: 10 }}
             />
             <Button mode="contained" onPress={() => console.log("press")} style={{ marginTop: 60 }}>
                 Login
@@ -24,3 +25,14 @@ export function Login(){
        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    wrapper: {
+        flex: 1,
+        width: "100%",
+        alignContent: "center",
+        justifyContent: "center",
+        paddingHorizontal: 15,
+
+    }
+})
