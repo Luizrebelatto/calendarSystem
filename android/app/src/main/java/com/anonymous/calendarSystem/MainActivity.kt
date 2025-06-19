309654355efdb2954e6c7ec04a0e39eddd2a1c66
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2025-06-18 21:44:32 -0300

    install mmkv and prebuild

--- android/app/src/main/java/com/anonymous/calendarSystem/MainActivity.kt
+++ android/app/src/main/java/com/anonymous/calendarSystem/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.calendarsystem
+package com.anonymous.calendarSystem
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/anonymous/calendarSystem/MainApplication.kt
+++ android/app/src/main/java/com/anonymous/calendarSystem/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.calendarsystem
+package com.anonymous.calendarSystem
 
 import android.app.Application
 import android.content.res.Configuration
